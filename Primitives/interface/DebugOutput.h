d0f28728ac2b9a8c7349b26c7ea40182aa952a30
Egor Yusov <egor.yusov@gmail.com>
2018-03-27 09:06:36 -0700

    Added comments

--- Primitives/interface/DebugOutput.h
+++ Primitives/interface/DebugOutput.h
@@ -44,9 +44,22 @@ enum class DebugMessageSeverity : Int32
     FatalError
 };
 
-using DebugMessageCallbackType = void(*)(DebugMessageSeverity, const Char* Message, const char* Function, const char* File, int Line);
+
+/// Type of the debug messag callback function
+
+/// \param [in] Severity - Message severity
+/// \param [in] Message - Debug message
+/// \param [in] Function - Name of the function or nullptr
+/// \param [in] Function - File name or nullptr
+/// \param [in] Line - Line number
+using DebugMessageCallbackType = void(*)(DebugMessageSeverity Severity, const Char* Message, const char* Function, const char* File, int Line);
 extern DebugMessageCallbackType DebugMessageCallback;
 
+
+/// Sets the debug message callback function
+
+/// \note This function needs to be called for every executable module that
+///       wants to use the callback.
 void SetDebugMessageCallback(DebugMessageCallbackType DbgMessageCallback);
 
 }
