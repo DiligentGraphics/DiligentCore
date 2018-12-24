604b9ef1a83ffc02eb90f48d5ca7bd5ed78ce8c4
Egor Yusov <egor.yusov@gmail.com>
2018-12-23 19:40:32 -0800

    Enabled Vulkan build on macOS

--- Graphics/GraphicsEngineVulkan/src/SwapChainVkImpl.cpp
+++ Graphics/GraphicsEngineVulkan/src/SwapChainVkImpl.cpp
@@ -57,12 +57,12 @@ SwapChainVkImpl::SwapChainVkImpl(IReferenceCounters*    pRefCounters,
 #elif defined(VK_USE_PLATFORM_IOS_MVK)
     VkIOSSurfaceCreateInfoMVK surfaceCreateInfo = {};
     surfaceCreateInfo.sType = VK_STRUCTURE_TYPE_IOS_SURFACE_CREATE_INFO_MVK;
-    surfaceCreateInfo.pView = nullptr;
+    surfaceCreateInfo.pView = pNativeWndHandle;
     auto err = vkCreateIOSSurfaceMVK(m_VulkanInstance->GetVkInstance(), &surfaceCreateInfo, nullptr, &m_VkSurface);
 #elif defined(VK_USE_PLATFORM_MACOS_MVK)
     VkMacOSSurfaceCreateInfoMVK surfaceCreateInfo = {};
     surfaceCreateInfo.sType = VK_STRUCTURE_TYPE_MACOS_SURFACE_CREATE_INFO_MVK;
-    surfaceCreateInfo.pView = nullptr;
+    surfaceCreateInfo.pView = pNativeWndHandle;
     auto err = vkCreateMacOSSurfaceMVK(m_VulkanInstance->GetVkInstance(), &surfaceCreateInfo, NULL, &m_VkSurface);
 #elif defined(VK_USE_PLATFORM_WAYLAND_KHR)
     VkWaylandSurfaceCreateInfoKHR surfaceCreateInfo = {};
