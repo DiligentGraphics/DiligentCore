be0b3ea5b175657001fd085df8238cb261a25b04
Egor Yusov <egor.yusov@gmail.com>
2019-11-23 21:52:47 -0800

    Moved format validation tools to Utilities/FormatValidation

--- BuildTools/validate_format_win.bat
+++
@@ -1,1 +0,0 @@
-python clang-format-validate.py --color never --clang-format-executable clang-format_10.0.0.exe -r ../Common ../Graphics ../Platforms ../Primitives
---
+++ Utilities/FormatValidation/validate_format_win.bat
@@ -0,0 +1,1 @@
+python clang-format-validate.py --color never --clang-format-executable clang-format_10.0.0.exe -r ../../Common ../../Graphics ../../Platforms ../../Primitives
