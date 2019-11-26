d45d86e47888ac7269d4f07d41305981618cc4d2
Egor Yusov <egor.yusov@gmail.com>
2019-11-26 10:47:35 -0800

    Fixed install.bat

--- BuildTools/Scripts/appveyor/install.bat
+++ BuildTools/Scripts/appveyor/install.bat
@@ -1,7 +1,7 @@
 set CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v3.15.4/cmake-3.15.4-win64-x64.zip"
 appveyor DownloadFile %CMAKE_URL% -FileName cmake.zip
 7z x cmake.zip -oC:\projects\deps > nul
-move C:\projects\deps\cmake-* C:\projects\deps\cmake # Move to a version-agnostic directory
+move C:\projects\deps\cmake-* C:\projects\deps\cmake
 set PATH=C:\projects\deps\cmake\bin;C:\Python37;%PATH%
 cmake --version
 python --version
