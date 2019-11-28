d5e4f0667835b44735e66a61bda1bcd53e03455e
Egor <egor.yusov@gmail.com>
2019-11-27 19:44:14 -0800

    Fixed build.sh

--- BuildTools/Scripts/travis/build.sh
+++ BuildTools/Scripts/travis/build.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   cmake .. -G "Unix Makefiles" $1 -DCMAKE_BUILD_TYPE=${CONFIG}
   cmake --build .
 fi
-|
+
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then 
   if [ "$IOS" = "true" ]; then 
     cmake .. -DCMAKE_TOOLCHAIN_FILE=../DiligentCore/ios.toolchain.cmake -DIOS_PLATFORM=SIMULATOR64 $1 -G "Xcode"
