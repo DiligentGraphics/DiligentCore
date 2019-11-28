30eb277da684d5c68781c5620041bfc0e37039f2
Egor <egor.yusov@gmail.com>
2019-11-27 20:52:21 -0800

    Updated info message from format validation script

--- BuildTools/FormatValidation/validate_format_linux.sh
+++ BuildTools/FormatValidation/validate_format_linux.sh
@@ -4,5 +4,5 @@
 #    --exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions_inc.h \
 #    --exclude ../../Graphics/GraphicsEngineVulkan/shaders/* \
 #    ../../Common ../../Graphics ../../Platforms ../../Primitives
-echo "Format validation is not yet enabled on Linux"
+echo "Format validation is not yet enabled for DiligentCore module on Linux"
 
