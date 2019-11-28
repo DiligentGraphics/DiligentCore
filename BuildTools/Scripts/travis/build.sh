ff447b57f5b364be1a4a5d7e07c8183998aa2112
Egor <egor.yusov@gmail.com>
2019-11-27 18:36:36 -0800

    Fixed travis build

--- BuildTools/Scripts/travis/build.sh
+++ BuildTools/Scripts/travis/build.sh
@@ -2,15 +2,15 @@ mkdir build
 cd build
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then 
-  cmake .. -G "Unix Makefiles" -DDILIGENT_ENABLE_TESTS=TRUE -DCMAKE_BUILD_TYPE=${CONFIG}
+  cmake .. -G "Unix Makefiles" $1 -DCMAKE_BUILD_TYPE=${CONFIG}
   cmake --build .
 fi
 |
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then 
   if [ "$IOS" = "true" ]; then 
-    cmake .. -DCMAKE_TOOLCHAIN_FILE=../DiligentCore/ios.toolchain.cmake -DIOS_PLATFORM=SIMULATOR64 -DDILIGENT_ENABLE_TESTS=TRUE -G "Xcode"
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=../DiligentCore/ios.toolchain.cmake -DIOS_PLATFORM=SIMULATOR64 $1 -G "Xcode"
   else
-    cmake .. -DDILIGENT_ENABLE_TESTS=TRUE -G "Xcode"
+    cmake .. $1 -G "Xcode"
   fi
   xcodebuild -configuration ${CONFIG} | xcpretty
 fi
