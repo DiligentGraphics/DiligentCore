a0eb8460a9436e368dfb8af7dffdb4811789ca4d
Egor Yusov <egor.yusov@gmail.com>
2019-12-07 10:33:01 -0800

    appveyor: fixed test running command

---
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set RUN_API_CORE_TEST=false
+set RUN_CORE_API_D3D11_TEST=false
+set set RUN_D3D12_TEST=false
+
+if "%PLATFORM_NAME%"=="Windows" (
+    set RUN_API_CORE_TEST=true
+    set RUN_CORE_API_D3D11_TEST=true
+    set RUN_CORE_API_D3D12_TEST=true
+)
+
+if "%PLATFORM_NAME%"=="Windows8.1" (
+    set RUN_API_CORE_TEST=true
+    set RUN_CORE_API_D3D11_TEST=true
+)
+
+if "%RUN_API_CORE_TEST%"=="true" (
+    "%1\UnitTests\DiligentCoreTest\%CONFIGURATION%\DiligentCoreTest.exe"
+)
+
+if "%RUN_CORE_API_D3D11_TEST%"=="true" (
+    "%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d11_sw
+)
+
+if "%RUN_CORE_API_D3D12_TEST%"=="true" (
+    "%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d12_sw
+)
