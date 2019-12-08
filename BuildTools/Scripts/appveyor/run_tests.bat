919ca9e9db0dc107c35efea3a04a7b4e07cbee4b
Egor Yusov <egor.yusov@gmail.com>
2019-12-08 12:58:44 -0800

    Updated run_test.bat to capture error in any of the test scripts

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -2,7 +2,8 @@ rem @echo off
 
 set RUN_API_CORE_TEST=false
 set RUN_CORE_API_D3D11_TEST=false
-set set RUN_D3D12_TEST=false
+set RUN_D3D12_TEST=false
+set ERROR=0
 
 if "%PLATFORM_NAME%"=="Windows" (
     set RUN_API_CORE_TEST=true
@@ -16,13 +17,15 @@ if "%PLATFORM_NAME%"=="Windows8.1" (
 )
 
 if "%RUN_API_CORE_TEST%"=="true" (
-    "%1\UnitTests\DiligentCoreTest\%CONFIGURATION%\DiligentCoreTest.exe"
+    "%1\UnitTests\DiligentCoreTest\%CONFIGURATION%\DiligentCoreTest.exe" || set ERROR=%ERRORLEVEL%
 )
 
 if "%RUN_CORE_API_D3D11_TEST%"=="true" (
-    "%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d11_sw
+    "%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d11_sw || set ERROR=%ERRORLEVEL%
 )
 
 if "%RUN_CORE_API_D3D12_TEST%"=="true" (
-    "%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d12_sw
+    "%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d12_sw || set ERROR=%ERRORLEVEL%
 )
+
+exit /B %ERROR% REM use /B to exit the current batch script context, and not the command prompt process
