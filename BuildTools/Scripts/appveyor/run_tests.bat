ab6b4ce15e7bc83f28b75b8ca8f234b6be7535f1
assiduous <assiduous@diligentgraphics.com>
2019-12-12 17:27:15 -0800

    Fixed run_tests.bat to avoid strange behavior of errorlevel

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -17,24 +17,15 @@ if "%PLATFORM_NAME%"=="Windows8.1" (
 )
 
 if "%RUN_API_CORE_TEST%"=="true" (
-    "%1\UnitTests\DiligentCoreTest\%CONFIGURATION%\DiligentCoreTest.exe"
-    if %ERRORLEVEL% NEQ 0 (
-        set ERROR=%ERRORLEVEL%
-    )
+    "%1\UnitTests\DiligentCoreTest\%CONFIGURATION%\DiligentCoreTest.exe" || set ERROR=1
 )
 
 if "%RUN_CORE_API_D3D11_TEST%"=="true" (
-    "%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d11_sw
-    if %ERRORLEVEL% NEQ 0 (
-        set ERROR=%ERRORLEVEL%
-    )
+    ("%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d11_sw) || set /a ERROR=%ERROR%+10
 )
 
 if "%RUN_CORE_API_D3D12_TEST%"=="true" (
-    "%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d12_sw
-    if %ERRORLEVEL% NEQ 0 (
-        set ERROR=%ERRORLEVEL%
-    )
+    ("%1\UnitTests\DiligentCoreAPITest\%CONFIGURATION%\DiligentCoreAPITest.exe" --mode=d3d12_sw) || set /a ERROR=%ERROR%+100
 )
 
 exit /B %ERROR% REM use /B to exit the current batch script context, and not the command prompt process
