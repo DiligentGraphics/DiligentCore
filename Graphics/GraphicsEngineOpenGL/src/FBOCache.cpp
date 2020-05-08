b82b0a5f247eba5d73d913dbd01a88cde2132368
Mark Nefedov <mark_nefedov@outlook.com>
2020-05-08 18:31:56 +0300

    The 'Key.Hash' variable was assigned the same value. ``` if (Key.Hash == 0){ Key.Hash = 0; } ```

--- Graphics/GraphicsEngineOpenGL/src/FBOCache.cpp
+++ Graphics/GraphicsEngineOpenGL/src/FBOCache.cpp
@@ -67,7 +67,6 @@ std::size_t FBOCache::FBOCacheKeyHashFunc::operator()(const FBOCacheKey& Key) co
     if (Key.Hash == 0)
     {
         std::hash<TextureViewDesc> TexViewDescHasher;
-        Key.Hash = 0;
         HashCombine(Key.Hash, Key.NumRenderTargets);
         for (Uint32 rt = 0; rt < Key.NumRenderTargets; ++rt)
         {
