a012234bf911a718cf43274d93dcc2b8bfae04ac
dpellegr <pellegrini.dario@gmail.com>
2020-08-07 10:35:03 +0200

    Validate format won't crash if it cannot load the provided binary

--- BuildTools/FormatValidation/validate_format_linux.sh
+++ BuildTools/FormatValidation/validate_format_linux.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
-python clang-format-validate.py --clang-format-executable ./clang-format_linux_10.0.0 \
--r ../../Common ../../Graphics ../../Platforms ../../Primitives ../../Tests \
---exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions.h \
---exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions_inc.h \
---exclude ../../Graphics/GraphicsEngineVulkan/shaders/GenerateMipsCS_inc.h \
---exclude ../../Tests/DiligentCoreAPITest/assets/*
+
+## Set the shipped BIN
+BIN="./clang-format_linux_10.0.0"
+## ...or use the find command to avoid maintaining
+#BIN=$(find . -name 'clang-format_linux*')
+
+## Set to anything but 0 to force skip format validation
+SKIP=0
+
+if [ "$SKIP" -eq 0 ]; then
+  ## Try to get the version
+  eval "$BIN" --version 2> /dev/null  
+  if [ $? -ne 0 ]; then
+    ## Try to get a system installed clang-format
+    BIN=$(which clang-format 2> /dev/null)
+    if [ $? -ne 0 ]; then
+      echo "WARNING: skipping format validation as no suitable executable was found"
+      SKIP=1
+    else
+      echo "WARNING: could not load the integrated format validator binary, but a system one was found."
+      echo "   Should it cause any issue due to versioning, you can skip validation by setting \"SKIP=1\""
+      echo "   in: DiligentCore/BuildTools/FormatValidation/validate_format_linux.sh"
+    fi
+  fi
+  
+  if [ "$SKIP" -eq 0 ]; then
+    python clang-format-validate.py --clang-format-executable "$BIN" \
+    -r ../../Common ../../Graphics ../../Platforms ../../Primitives ../../Tests \
+    --exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions.h \
+    --exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions_inc.h \
+    --exclude ../../Graphics/GraphicsEngineVulkan/shaders/GenerateMipsCS_inc.h \
+    --exclude ../../Tests/DiligentCoreAPITest/assets/*
+  fi
+fi
