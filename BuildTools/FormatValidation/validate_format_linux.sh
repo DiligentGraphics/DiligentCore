c29a3408e4bfbe508a3cd1a6432135ef7b812a38
dpellegr <pellegrini.dario@gmail.com>
2020-08-09 11:46:05 +0200

    Separated implementation of validate format linux allowing reuse in submodules

--- BuildTools/FormatValidation/validate_format_linux.sh
+++ BuildTools/FormatValidation/validate_format_linux.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 
-BIN=$(find . -name 'clang-format_linux_*')
+source validate_format_linux_implementation.sh
 
-## Try to launch the bin
-eval "$BIN --version >/dev/null 2> /dev/null"
-if [ $? -ne 0 ]; then
-  ## BIN failed to run, try to get a system installed clang-format
-  SYS_BIN=$(which clang-format 2> /dev/null)
-  if [ $? -ne 0 ]; then
-    echo "WARNING: skipping format validation as no suitable executable was found"
-    BIN=""
-  else
-    BIN_VERSION=$(echo $BIN | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
-    SYS_BIN_VERSION=$(eval "$SYS_BIN --version" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
-    if [ "$BIN_VERSION" != "$SYS_BIN_VERSION" ]; then
-      echo "WARNING: could not load the provided clang-format for validation."
-      echo "   clang-format exists in the system path however its version is $SYS_BIN_VERSION instead of $BIN_VERSION"
-      echo "   Should the validation fail, you can try skipping it by setting the cmake option:"
-      echo "   DILIGENT_SKIP_FORMAT_VALIDATION"
-    fi
-    BIN="$SYS_BIN"
-  fi
-fi
-
-if [ ! -z "$BIN" ]; then
-  python clang-format-validate.py --clang-format-executable "$BIN" \
-  -r ../../Common ../../Graphics ../../Platforms ../../Primitives ../../Tests \
+validate_format ../../Common ../../Graphics ../../Platforms ../../Primitives ../../Tests \
   --exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions.h \
   --exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions_inc.h \
   --exclude ../../Graphics/GraphicsEngineVulkan/shaders/GenerateMipsCS_inc.h \
   --exclude ../../Tests/DiligentCoreAPITest/assets/*
-fi
+
