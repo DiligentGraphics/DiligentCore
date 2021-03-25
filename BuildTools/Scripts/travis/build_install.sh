5430f67fbcc486e9b3a42d3165f4c93d9675d41f
assiduous <assiduous@diligentgraphics.com>
2021-03-25 01:03:06 -0700

    travis: disabled using old build system on Mac/iOS (https://github.com/DiligentGraphics/DiligentEngine/issues/119)

--- BuildTools/Scripts/travis/build_install.sh
+++ BuildTools/Scripts/travis/build_install.sh
@@ -10,10 +10,10 @@ fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then 
   if [ "$IOS" = "true" ]; then 
-    cmake .. $1 -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 -DVULKAN_SDK="$VULKAN_SDK" -DCMAKE_INSTALL_PREFIX=install -G "Xcode" -T buildsystem=1 || return
+    cmake .. $1 -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 -DVULKAN_SDK="$VULKAN_SDK" -DCMAKE_INSTALL_PREFIX=install -G "Xcode" || return
     XCODE_BUILD_SETTINGS="CODE_SIGN_IDENTITY= CODE_SIGNING_REQUIRED=NO"
   else
-    cmake .. $1 -DCMAKE_INSTALL_PREFIX=install -G "Xcode" -T buildsystem=1 || return
+    cmake .. $1 -DCMAKE_INSTALL_PREFIX=install -G "Xcode" || return
     XCODE_BUILD_SETTINGS=""
   fi
   xcodebuild -target install -configuration ${CONFIG} ${XCODE_BUILD_SETTINGS} | xcpretty && return ${PIPESTATUS[0]}
