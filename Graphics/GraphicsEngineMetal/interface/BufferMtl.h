478da1d4cc61467e28a73e953658f4ecafcd686b
assiduous <assiduous@diligentgraphics.com>
2021-05-11 12:45:01 -0700

    Updated copyright notice for Metal interface headers

--- Graphics/GraphicsEngineMetal/interface/BufferMtl.h
+++ Graphics/GraphicsEngineMetal/interface/BufferMtl.h
@@ -1,6 +1,5 @@
 /*
  *  Copyright 2019-2021 Diligent Graphics LLC
- *  Copyright 2015-2019 Egor Yusov
  *  
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
