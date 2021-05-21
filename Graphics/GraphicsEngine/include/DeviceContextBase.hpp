42d3b141d406acd8c7d393fddaa4a540b44101b7
azhirnov <zh1dron@gmail.com>
2021-05-21 19:18:07 +0300

    Check alignment for VertexOffset and IndexOffset BLAS build attribs

--- Graphics/GraphicsEngine/include/DeviceContextBase.hpp
+++ Graphics/GraphicsEngine/include/DeviceContextBase.hpp
@@ -82,7 +82,7 @@ bool VerifyStateTransitionDesc(const IRenderDevice*       pDevice,
                                DeviceContextIndex         ExecutionCtxId,
                                const DeviceContextDesc&   CtxDesc);
 
-bool VerifyBuildBLASAttribs(const BuildBLASAttribs& Attribs);
+bool VerifyBuildBLASAttribs(const IRenderDevice* pDevice, const BuildBLASAttribs& Attribs);
 bool VerifyBuildTLASAttribs(const BuildTLASAttribs& Attribs);
 bool VerifyCopyBLASAttribs(const IRenderDevice* pDevice, const CopyBLASAttribs& Attribs);
 bool VerifyCopyTLASAttribs(const CopyTLASAttribs& Attribs);
@@ -1727,7 +1727,7 @@ void DeviceContextBase<ImplementationTraits>::BuildBLAS(const BuildBLASAttribs& 
     DVP_CHECK_QUEUE_TYPE_COMPATIBILITY(COMMAND_QUEUE_TYPE_COMPUTE, "BuildBLAS");
     DEV_CHECK_ERR(m_pDevice->GetFeatures().RayTracing, "IDeviceContext::BuildBLAS: ray tracing is not supported by this device");
     DEV_CHECK_ERR(m_pActiveRenderPass == nullptr, "IDeviceContext::BuildBLAS command must be performed outside of render pass");
-    DEV_CHECK_ERR(VerifyBuildBLASAttribs(Attribs), "BuildBLASAttribs are invalid");
+    DEV_CHECK_ERR(VerifyBuildBLASAttribs(m_pDevice, Attribs), "BuildBLASAttribs are invalid");
 }
 
 template <typename ImplementationTraits>
