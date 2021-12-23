15bd593ad0cbdb735b9f5b597dafda784b3b654c
azhirnov <zh1dron@gmail.com>
2021-12-23 13:54:35 +0300

    Added SHADER_SOURCE_LANGUAGE_MTLB

--- Graphics/GraphicsEngine/interface/Shader.h
+++ Graphics/GraphicsEngine/interface/Shader.h
@@ -56,9 +56,6 @@ DILIGENT_TYPED_ENUM(SHADER_SOURCE_LANGUAGE, Uint32)
     /// The source language is GLSL
     SHADER_SOURCE_LANGUAGE_GLSL,
 
-    /// The source language is Metal shading language (MSL)
-    SHADER_SOURCE_LANGUAGE_MSL,
-
     /// The source language is GLSL that should be compiled verbatim
 
     /// By default the engine prepends GLSL shader source code with platform-specific
@@ -67,7 +64,13 @@ DILIGENT_TYPED_ENUM(SHADER_SOURCE_LANGUAGE, Uint32)
     /// When SHADER_SOURCE_LANGUAGE_GLSL_VERBATIM is used, the source code will be compiled as is.
     /// Note that shader macros are ignored when compiling GLSL verbatim in OpenGL backend, and an application
     /// should add the macro definitions to the source code.
-    SHADER_SOURCE_LANGUAGE_GLSL_VERBATIM
+    SHADER_SOURCE_LANGUAGE_GLSL_VERBATIM,
+
+    /// The source language is Metal shading language (MSL)
+    SHADER_SOURCE_LANGUAGE_MSL,
+
+    /// The source language is Metal bytecode
+    SHADER_SOURCE_LANGUAGE_MTLB
 };
 
 
