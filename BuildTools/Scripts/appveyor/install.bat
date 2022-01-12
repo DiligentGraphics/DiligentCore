1abbc011033dfc4d1380b50a6546a3f37bb73dde
MikhailGorobets <misha.friman@mail.ru>
2022-01-12 19:46:42 +0500

    CI: Switch to Python3.7-x64

--- BuildTools/Scripts/appveyor/install.bat
+++ BuildTools/Scripts/appveyor/install.bat
@@ -2,6 +2,6 @@ set CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-
 appveyor DownloadFile %CMAKE_URL% -FileName cmake.zip
 7z x cmake.zip -oC:\projects\deps > nul
 move C:\projects\deps\cmake-* C:\projects\deps\cmake
-set PATH=C:\projects\deps\cmake\bin;C:\Python37;%PATH%
+set PATH=C:\projects\deps\cmake\bin;C:\Python37-x64;%PATH%
 cmake --version
 python --version
