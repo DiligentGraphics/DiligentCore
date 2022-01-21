068f259d6fffb7fa3172ce6f6ab780d1b6c6369f
assiduous <assiduous@diligentgraphics.com>
2022-01-20 19:28:12 -0800

    PipelineResourceSignatureTest.CombinedImageSamplers_HLSL: fixed non-separable GL programs case

--- Tests/DiligentCoreAPITest/assets/shaders/PipelineResourceSignature/CombinedImageSamplers.hlsl
+++ Tests/DiligentCoreAPITest/assets/shaders/PipelineResourceSignature/CombinedImageSamplers.hlsl
@@ -63,8 +63,8 @@ void VSMain(in  uint    VertId    : SV_VertexID,
     f4Position = Pos[VertId];
 }
 
-float4 PSMain(in float4 in_f4Color : COLOR,
+float4 PSMain(in float4 f4Color    : COLOR,
               in float4 f4Position : SV_Position) : SV_Target
 {
-    return in_f4Color * VerifyResources();
+    return f4Color * VerifyResources();
 }
