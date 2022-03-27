43d0d04cc780493d12f9f1b4c255c31f22d4bd23
assiduous <assiduous@diligentgraphics.com>
2022-03-26 21:06:47 -0700

    Format validation script: use 'python3' instead of 'python' on Mac

--- BuildTools/FormatValidation/validate_format_mac.sh
+++ BuildTools/FormatValidation/validate_format_mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python clang-format-validate.py --clang-format-executable ./clang-format_mac_10.0.0 \
+python3 clang-format-validate.py --clang-format-executable ./clang-format_mac_10.0.0 \
 -r ../../Common ../../Graphics ../../Platforms ../../Primitives ../../Tests \
 --exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions.h \
 --exclude ../../Graphics/HLSL2GLSLConverterLib/include/GLSLDefinitions_inc.h \
