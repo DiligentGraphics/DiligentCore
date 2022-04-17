dd7397000db8fc925a468251d1c9b56133212f06
assiduous <assiduous@diligentgraphics.com>
2022-04-16 21:02:53 -0700

    GitHub actions: added ninja configuration for windows (close #328)

--- BuildTools/Scripts/github_actions/configure_cmake.sh
+++ BuildTools/Scripts/github_actions/configure_cmake.sh
@@ -1,14 +1,26 @@
+
 # $1 - platform name
 # $2 - runner workspace
-# $3 - config
-# $4 - toolset (MSVC)
+
+# Windows:
+#   $3 - platform (x64, Win32)
+#   $4 - config (Debug, Release, etc.)
+#   $5 - extra args
+
+# Other platforms:
+#   $3 - config (Debug, Release, etc.)
+#   $4 - extra args
 
 cd "$2"
 mkdir build
 cd ./build
 
 if [ "$1" = "win" ]; then
-  cmake $GITHUB_WORKSPACE -G "Visual Studio 16 2019" -A $3 -DCMAKE_BUILD_TYPE=$4 $5 -DDILIGENT_NO_FORMAT_VALIDATION=ON -DCMAKE_INSTALL_PREFIX=install
+  if [ "$CMAKE_GENERATOR" == "" ]; then
+    cmake $GITHUB_WORKSPACE -G "Visual Studio 16 2019" -A $3 -DCMAKE_BUILD_TYPE=$4 $5 -DDILIGENT_NO_FORMAT_VALIDATION=ON -DCMAKE_INSTALL_PREFIX=install
+  else
+    cmake $GITHUB_WORKSPACE -G "$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$4 $5 -DDILIGENT_NO_FORMAT_VALIDATION=ON -DCMAKE_INSTALL_PREFIX=install
+  fi
 fi
 
 if [ "$1" = "uwp" ]; then
