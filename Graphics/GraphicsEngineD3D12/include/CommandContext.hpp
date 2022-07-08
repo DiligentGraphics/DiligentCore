6712ee4a3a823515bcee182a8bca299dc00db708
assiduous <assiduous@diligentgraphics.com>
2022-07-08 14:56:13 -0700

    Added ScopedDebugGroup helper class

--- Graphics/GraphicsEngineD3D12/include/CommandContext.hpp
+++ Graphics/GraphicsEngineD3D12/include/CommandContext.hpp
@@ -239,9 +239,14 @@ public:
     void PixSetMarker(const Char* Label, const float* pColor);
 #else
     void PixBeginEvent(const Char* Name, const float* pColor)
-    {}
+    {
+        LOG_WARNING_MESSAGE_ONCE("Diligent Engine was built without PIX support. Use DILIGENT_LOAD_PIX_EVENT_RUNTIME CMake option to enable it.");
+    }
     void PixEndEvent() {}
-    void PixSetMarker(const Char* Label, const float* pColor) {}
+    void PixSetMarker(const Char* Label, const float* pColor)
+    {
+        LOG_WARNING_MESSAGE_ONCE("Diligent Engine was built without PIX support. Use DILIGENT_LOAD_PIX_EVENT_RUNTIME CMake option to enable it.");
+    }
 #endif
 
 protected:
