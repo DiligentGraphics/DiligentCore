d84a78ca1f5f08611f2eff1e929b80f2447aab67
assiduous <assiduous@diligentgraphics.com>
2022-11-01 15:28:10 -0700

    Fixed CopyStaticResourcesCS test on Metal

--- Tests/DiligentCoreAPITest/assets/shaders/ShaderResourceLayout/CopyStaticResourcesCS.glsl
+++ Tests/DiligentCoreAPITest/assets/shaders/ShaderResourceLayout/CopyStaticResourcesCS.glsl
@@ -1,18 +1,18 @@
 layout(std140, binding = 0) buffer g_RWBuff_0
 {
     vec4 data[4];
-}g_StorageBuff_0;
+}g_StorageBuff0;
 
 layout(std140, binding = 1) buffer g_RWBuff_1
 {
     vec4 data[4];
-}g_StorageBuff_1;
+}g_StorageBuff1;
 
 
 layout(std140, binding = 2) buffer g_RWBuff_2
 {
     vec4 data[4];
-}g_StorageBuff_2;
+}g_StorageBuff2;
 
 layout(rgba8, binding = 3) uniform image2D g_RWTex2D_0;
 layout(rgba8, binding = 4) uniform image2D g_RWTex2D_1;
@@ -31,15 +31,15 @@ vec4 VerifyResources()
     vec4 AllCorrect = vec4(1.0, 1.0, 1.0, 1.0);
 
     // Read from elements 1,2,3
-    AllCorrect *= CheckValue(g_StorageBuff_0.data[1], Buff_0_Ref);
-    AllCorrect *= CheckValue(g_StorageBuff_1.data[2], Buff_1_Ref);
-    AllCorrect *= CheckValue(g_StorageBuff_2.data[3], Buff_2_Ref);
+    AllCorrect *= CheckValue(g_StorageBuff0.data[1], Buff_0_Ref);
+    AllCorrect *= CheckValue(g_StorageBuff1.data[2], Buff_1_Ref);
+    AllCorrect *= CheckValue(g_StorageBuff2.data[3], Buff_2_Ref);
 
     // Write to 0-th element
     vec4 Data = vec4(1.0, 2.0, 3.0, 4.0);
-    g_StorageBuff_0.data[0] = Data;
-    g_StorageBuff_1.data[0] = Data;
-    g_StorageBuff_2.data[0] = Data;
+    g_StorageBuff0.data[0] = Data;
+    g_StorageBuff1.data[0] = Data;
+    g_StorageBuff2.data[0] = Data;
 
     AllCorrect *= CheckValue(imageLoad(g_RWTex2D_0, ivec2(10, 12)), RWTex2D_0_Ref);
     AllCorrect *= CheckValue(imageLoad(g_RWTex2D_1, ivec2(14, 17)), RWTex2D_1_Ref);
