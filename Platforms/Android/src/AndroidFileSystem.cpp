880398320cdd19b90717b69e0d4cc08c56b75c7a
assiduous <assiduous@diligentgraphics.com>
2023-09-05 13:05:05 -0700

    Android file system: fixed GetLocalAppDataDirectory not being a struct member

--- Platforms/Android/src/AndroidFileSystem.cpp
+++ Platforms/Android/src/AndroidFileSystem.cpp
@@ -365,7 +365,7 @@ std::vector<std::unique_ptr<FindFileData>> AndroidFileSystem::Search(const Char*
     return std::vector<std::unique_ptr<FindFileData>>();
 }
 
-std::string GetLocalAppDataDirectory(const char* AppName /*= nullptr*/, bool Create /*= true*/)
+std::string AndroidFileSystem::GetLocalAppDataDirectory(const char* AppName /*= nullptr*/, bool Create /*= true*/)
 {
     UNSUPPORTED("GetLocalAppDataDirectory() is not supported on Android");
     return "";
