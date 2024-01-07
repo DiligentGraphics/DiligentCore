a28ae843ed9977559b3ed50f11fe701e17e5dcc6
assiduous <assiduous@diligentgraphics.com>
2024-01-06 18:18:18 -0800

    Added StaticCast overload for RefCntAutoPtr
    Also moved methods in the Cast.hpp header to Diligent namespace

--- Common/interface/RefCntAutoPtr.hpp
+++ Common/interface/RefCntAutoPtr.hpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2019-2023 Diligent Graphics LLC
+ *  Copyright 2019-2024 Diligent Graphics LLC
  *  Copyright 2015-2019 Egor Yusov
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -447,5 +447,11 @@ protected:
     // a pointer to the owner, which is not what we need.
     T* m_pObject;
 };
+
+template <typename DstType, typename SrcType>
+NODISCARD DstType StaticCast(const RefCntAutoPtr<SrcType>& Src)
+{
+    return static_cast<DstType>(Src.RawPtr());
+}
 
 } // namespace Diligent
