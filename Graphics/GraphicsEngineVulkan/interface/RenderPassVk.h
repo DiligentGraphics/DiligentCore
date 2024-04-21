078d68e68b5184be8f8979b491f2a7a4c3bad02d
assiduous <assiduous@diligentgraphics.com>
2024-04-21 14:18:43 -0700

    Fixed a couple of minor issues in ISamplerVk and IRenderPassVk interfaces

--- Graphics/GraphicsEngineVulkan/interface/RenderPassVk.h
+++ Graphics/GraphicsEngineVulkan/interface/RenderPassVk.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2019-2023 Diligent Graphics LLC
+ *  Copyright 2019-2024 Diligent Graphics LLC
  *  Copyright 2015-2019 Egor Yusov
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -49,7 +49,7 @@ static DILIGENT_CONSTEXPR INTERFACE_ID IID_RenderPassVk =
 DILIGENT_BEGIN_INTERFACE(IRenderPassVk, IRenderPass)
 {
     /// Returns a Vulkan handle of the internal render pass object.
-    VIRTUAL VkRenderPass METHOD(GetVkRenderPass)() CONST PURE;
+    VIRTUAL VkRenderPass METHOD(GetVkRenderPass)(THIS) CONST PURE;
 };
 DILIGENT_END_INTERFACE
 
--- Graphics/GraphicsEngineVulkan/interface/SamplerVk.h
+++ Graphics/GraphicsEngineVulkan/interface/SamplerVk.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2019-2023 Diligent Graphics LLC
+ *  Copyright 2019-2024 Diligent Graphics LLC
  *  Copyright 2015-2019 Egor Yusov
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -49,7 +49,7 @@ static DILIGENT_CONSTEXPR INTERFACE_ID IID_SamplerVk =
 DILIGENT_BEGIN_INTERFACE(ISamplerVk, ISampler)
 {
     /// Returns a Vulkan handle of the internal sampler object.
-    VIRTUAL VkSampler METHOD(GetVkSampler)() CONST PURE;
+    VIRTUAL VkSampler METHOD(GetVkSampler)(THIS) CONST PURE;
 };
 DILIGENT_END_INTERFACE
 
