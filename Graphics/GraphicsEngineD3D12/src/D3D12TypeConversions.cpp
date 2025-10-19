dcf15aee30461a93f5ab8b085518ec762a0433cd
assiduous <assiduous@diligentgraphics.com>
2025-10-19 10:13:43 -0700

    Added SHADER_RESOURCE_TYPE_INLINE_CONSTANTS enum value (#672)

--- Graphics/GraphicsEngineD3D12/src/D3D12TypeConversions.cpp
+++ Graphics/GraphicsEngineD3D12/src/D3D12TypeConversions.cpp
@@ -849,20 +849,25 @@ DXGI_FORMAT TypeToRayTracingVertexFormat(VALUE_TYPE ValueType, Uint32 ComponentC
 
 D3D12_DESCRIPTOR_RANGE_TYPE ResourceTypeToD3D12DescriptorRangeType(SHADER_RESOURCE_TYPE ResType)
 {
-    static_assert(SHADER_RESOURCE_TYPE_LAST == 8, "Please update the switch below to handle the new resource type");
+    static_assert(SHADER_RESOURCE_TYPE_LAST == 9, "Please update the switch below to handle the new resource type");
 
     switch (ResType)
     {
-        // clang-format off
-        case SHADER_RESOURCE_TYPE_CONSTANT_BUFFER: return D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
-        case SHADER_RESOURCE_TYPE_TEXTURE_SRV:     return D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
-        case SHADER_RESOURCE_TYPE_BUFFER_SRV:      return D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
-        case SHADER_RESOURCE_TYPE_TEXTURE_UAV:     return D3D12_DESCRIPTOR_RANGE_TYPE_UAV;
-        case SHADER_RESOURCE_TYPE_BUFFER_UAV:      return D3D12_DESCRIPTOR_RANGE_TYPE_UAV;
-        case SHADER_RESOURCE_TYPE_SAMPLER:         return D3D12_DESCRIPTOR_RANGE_TYPE_SAMPLER;
-        case SHADER_RESOURCE_TYPE_ACCEL_STRUCT:    return D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
-        case SHADER_RESOURCE_TYPE_INPUT_ATTACHMENT:return D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
-        // clang-format on
+            // clang-format off
+        case SHADER_RESOURCE_TYPE_CONSTANT_BUFFER:  return D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
+        case SHADER_RESOURCE_TYPE_TEXTURE_SRV:      return D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+        case SHADER_RESOURCE_TYPE_BUFFER_SRV:       return D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+        case SHADER_RESOURCE_TYPE_TEXTURE_UAV:      return D3D12_DESCRIPTOR_RANGE_TYPE_UAV;
+        case SHADER_RESOURCE_TYPE_BUFFER_UAV:       return D3D12_DESCRIPTOR_RANGE_TYPE_UAV;
+        case SHADER_RESOURCE_TYPE_SAMPLER:          return D3D12_DESCRIPTOR_RANGE_TYPE_SAMPLER;
+        case SHADER_RESOURCE_TYPE_ACCEL_STRUCT:     return D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+        case SHADER_RESOURCE_TYPE_INPUT_ATTACHMENT: return D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+            // clang-format on
+
+        case SHADER_RESOURCE_TYPE_INLINE_CONSTANTS:
+            UNEXPECTED("Inline constants do not have a descriptor range type");
+            return static_cast<D3D12_DESCRIPTOR_RANGE_TYPE>(-1);
+
         default:
             UNEXPECTED("Unknown resource type");
             return static_cast<D3D12_DESCRIPTOR_RANGE_TYPE>(-1);
