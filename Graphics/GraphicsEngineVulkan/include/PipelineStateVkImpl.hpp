01af99239bb15ad3f7667febe4eb00edd27ba943
hzqst <113660872@qq.com>
2026-02-26 20:49:46 -0800

    PipelineStateVk: build specialization constant data

--- Graphics/GraphicsEngineVulkan/include/PipelineStateVkImpl.hpp
+++ Graphics/GraphicsEngineVulkan/include/PipelineStateVkImpl.hpp
@@ -49,6 +49,11 @@ namespace Diligent
 
 class DeviceContextVkImpl;
 
+namespace
+{
+struct ShaderStageSpecializationData;
+}
+
 /// Pipeline state object implementation in Vulkan backend.
 class PipelineStateVkImpl final : public PipelineStateBase<EngineVkImplTraits>
 {
@@ -131,7 +136,8 @@ private:
     template <typename PSOCreateInfoType>
     TShaderStages InitInternalObjects(const PSOCreateInfoType&                           CreateInfo,
                                       std::vector<VkPipelineShaderStageCreateInfo>&      vkShaderStages,
-                                      std::vector<VulkanUtilities::ShaderModuleWrapper>& ShaderModules) noexcept(false);
+                                      std::vector<VulkanUtilities::ShaderModuleWrapper>& ShaderModules,
+                                      std::vector<ShaderStageSpecializationData>&        SpecDataPerStage) noexcept(false);
 
     void InitPipelineLayout(const PipelineStateCreateInfo& CreateInfo,
                             TShaderStages&                 ShaderStages) noexcept(false);
